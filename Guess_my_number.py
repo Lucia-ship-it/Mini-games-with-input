2d6de6340c916317fe4c20431f890751c90a7bed
ella <elalek@gmail.com>
2025-06-12 22:40:22 +0200

    zakladna hra

---
+++ Guess_my_number.py
@@ -0,0 +1,41 @@
+
+print("\nHi, I'm Bill\n")
+
+print("Let's play a game: guess my number. Can you figure it out?\n")
+
+print("I'm thinking of a number between 1 and 10.\n")
+
+while True:
+    my_secret_number = 7
+    your_guess = int(input("What number am I thinking of? "))
+
+    while my_secret_number != your_guess:
+        print(f"That's not {your_guess}. Try again!\n")
+        your_guess = int(input("What number am I thinking of? "))
+
+    print(f"\nYes, you got it! It really was number {my_secret_number}!\n\n")
+
+    print("Do you want to play again? Type yes or no:")
+
+    while True:
+        play_again = input().strip().lower()
+
+        if play_again == "yes":
+            print("\n\n\n")
+            print("Great, let's do it!")
+            print("\n\n\n")
+            break
+        elif play_again == "no":
+            print("\n\n\n")
+            print("Thanks for playing! Goodbye!\n")
+            exit
+        else:
+            print("Just type 'yes' or 'no'.")
+          
+
+# 1. dorobit - pocitadlo neplatnych pokusov s vylepsenymi hlaskami
+    #"Soo?: ")  print(f"Hey dude, Come on, it's just 'yes' or 'no'!\n{again}")
+# 2. random vyber cisla pocitacom
+# 3. postupne zobrazovanie textu, s oneskorenim pri hlaske o uhadnuti cisla
+
+
