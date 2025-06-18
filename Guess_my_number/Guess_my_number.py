f5d8bb0cf8b58f56b69a545239d1e7fd8022855d
ella <elalek@gmail.com>
2025-06-18 14:13:12 +0200

    osetrenie necislenych vstupov

--- Guess_my_number/Guess_my_number.py
+++ Guess_my_number/Guess_my_number.py
@@ -3,21 +3,30 @@ print("\nHi, I'm Bill\n")
 
 print("Let's play a game: guess my number. Can you figure it out?\n")
 
-
 import random
-
 
 while True:
     print("I'm thinking of a number between 1 and 10.\n")
     my_secret_number = random.randint(1, 10)
-    your_guess = int(input("What number am I thinking of? "))
+
+    while True:
+        try:
+            your_guess = int(input("\nWhat number am I thinking of?: "))
+            break
+        except ValueError: 
+            print("You need to type a number.\n")
 
     while my_secret_number != your_guess:
-        print(f"That's not {your_guess}. Try again!\n")
-        your_guess = int(input("What number am I thinking of? "))
+        print(f"That's not {your_guess}. Try again!\n")  
+       
+        while True:
+            try:
+                your_guess = int(input("What number am I thinking of?: "))
+                break
+            except ValueError: 
+                print("You need to type a number.\n")
 
     print(f"\nYes, you got it! It really was number {my_secret_number}!\n\n")
-
     print("Do you want to play again? Type yes or no:")
 
     while True:
@@ -31,18 +40,22 @@ while True:
         elif play_again == "no":
             print("\n\n\n")
             print("Thanks for playing! Goodbye!\n")
-            exit
+            exit()
         else:
             print("Just type 'yes' or 'no'.")
+
+            
           
         
 # OK    slucka na yes/no
-#       pocitadlo neplatnych pokusov s vylepsenymi hlaskami
-#       "Soo?: ")  print(f"Hey dude, Come on, it's just 'yes' or 'no'!\n{again}")
+# OK    osetrenie neciselnych vstupov
+#   obmedzenie na čísla v danom rozmedzi
+#    pocitadlo neplatnych pokusov s vylepsenymi hlaskami
+#   "Soo?: ")  print(f"Hey dude, Come on, it's just 'yes' or 'no'!\n{again}")
 # OK    vyber nahodneho cisla pocitacom
-#       postupne zobrazovanie textu, s oneskorenim pri hlaske o uhadnuti cisla
-#       skóre
-#       počet pokusov
-#       výber úrovne obtiažnosti
-#       Udrží konzolu otvorenú
+#   postupne zobrazovanie textu, s oneskorenim pri hlaske o uhadnuti cisla
+#     skóre
+#     počet pokusov
+#     výber úrovne obtiažnosti
+#     Udrží konzolu otvorenú
 
